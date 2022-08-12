d3017f2b57bbfa2febd4f0b246956c2ee15f7902
GermanDG <gdelgado@civitfun.com>
2022-08-12 15:43:41 +0100

    fizzBuzz resolved with map

--- src/fizz-buzz/my-solutions/map.fizzbuzz.spec.ts
+++ src/fizz-buzz/my-solutions/map.fizzbuzz.spec.ts
@@ -1,28 +1,28 @@
 import { fizzBuzz } from './map.fizzbuzz'
-describe('FizzBuzz', () => {
-  it('given a number must return a list starting at 1 and whose length is the same as the number.', () => {
+
+describe('FizzBuzz with map', () => {
+  it('given a number should return a list that starting on 1 and whose length is the same as the given number', () => {
     const given = 2
 
     const result = fizzBuzz(given)
 
     expect(result).toEqual([1, 2])
   })
-  it('If any number is divisible by 3 should replace the number with "fizz"', () => {
+  it('Every time a number is divisible by 3 replace the number with "fizz"', () => {
     const given = 3
 
     const result = fizzBuzz(given)
 
     expect(result).toEqual([1, 2, 'fizz'])
   })
-
-  it('If any number is divisible by 5 should replace with "buzz"', () => {
+  it('Every time a number is divisible by 5 replace the number with "fuzz"', () => {
     const given = 5
 
     const result = fizzBuzz(given)
 
     expect(result).toEqual([1, 2, 'fizz', 4, 'buzz'])
   })
-  it('', () => {
+  it('Every time a number is divisible by 3 and 5 replace the number with "fizzbuzz"', () => {
     const given = 15
 
     const result = fizzBuzz(given)
